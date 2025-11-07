53b43e1c57274c546a2e899e54332d2c4492c41d
lazaaq <lana.saiful.aqil@mail.ugm.ac.id>
2025-11-08 04:58:28 +0700

    feature: add axios in frontend

---
+++ frontend/src/api/axios.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
+
+// Attach token automatically if present
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+// handle 401 (token expired)
+api.interceptors.response.use(
+  (res) => res,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem("token"); 
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
